84480be1922237b11ba8bbb701e0e888caa7b5f9
समीर सिंह Sameer Singh <lumarzeli30@gmail.com>
2024-06-14 01:50:59 +0530

    Added LICENSE

--- src/gap_buffer.c
+++ src/gap_buffer.c
@@ -1,3 +1,11 @@
+/* This file is part of gapbuffer.
+
+gapbuffer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+gapbuffer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with gapbuffer. If not, see <https://www.gnu.org/licenses/>. */
+
 #include "../include/gap_buffer.h"
 
 #include <stdbool.h>
